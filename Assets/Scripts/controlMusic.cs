944ec69ba812fde43f53c5c83f8fb980a2125f16
Dylan Ou <dylan.ou.5597@gmail.com>
2020-11-28 16:54:18 -0600

    Added audio and animations to people

---
+++ Assets/Scripts/controlMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlMusic : MonoBehaviour
+{
+    public GameObject[] speakers;
+
+    AudioSource[] audioSources;
+
+    private void Start()
+    {
+        audioSources = new AudioSource[speakers.Length];
+
+        for(int i = 0; i < speakers.Length; i++)
+        {
+            audioSources[i] = speakers[i].GetComponent<AudioSource>();
+        }
+    }
+
+    public void onPress()
+    {
+        for(int i = 0; i < audioSources.Length; i++)
+        {
+            if (!audioSources[i].isPlaying)
+            {
+                audioSources[i].Play();
+            }
+            else
+            {
+                audioSources[i].Stop();
+            }
+        }
+    }
+}
