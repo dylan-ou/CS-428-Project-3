ea42fc54596d441a18d7268f17a455f6a3b51f57
Dylan Ou <dylan.ou.5597@gmail.com>
2020-11-28 05:03:04 -0600

    Added people

---
+++ Assets/Scripts/controlDancing.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlDancing : MonoBehaviour
+{
+    public GameObject[] people;
+
+    Animator[] animators;
+
+    bool isDancing = false;
+
+    private void Start()
+    {
+        animators = new Animator[people.Length];
+
+        for(int i = 0; i < people.Length; i++)
+        {
+            animators[i] = people[i].GetComponent<Animator>();    
+        }
+    }
+
+    public void controlDance()
+    {
+        if (!isDancing)
+        {
+            for(int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("idleToDance", true);
+            }
+            isDancing = true;
+        } else
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("danceToIdle", true);
+            }
+            isDancing = false;
+        }
+    }
+
+    public void resetBools()
+    {
+        if (!isDancing)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("idleToDance", false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].SetBool("danceToIdle", false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/controlScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlScreen : MonoBehaviour
+{
+    public GameObject gifs;
+
+    bool gifOn = false;
+
+    public void changeCurrentState()
+    {
+        gifOn = !gifOn;
+        gifs.SetActive(gifOn);
+    }
+}
