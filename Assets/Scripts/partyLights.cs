3ee5f96235d2691784b0dee4632e3f8dd0f8390f
Dylan Ou <dylan.ou.5597@gmail.com>
2020-11-28 18:00:03 -0600

    People done

---
+++ Assets/Scripts/partyLights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class partyLights : MonoBehaviour
+{
+    public GameObject[] arrayOfPartyLights;
+
+    public bool isOn = false;
+
+    public void turnOnLights()
+    {
+        if (!isOn)
+        {
+            isOn = true;
+            for (int i = 0; i < arrayOfPartyLights.Length; i++)
+            {
+                arrayOfPartyLights[i].SetActive(isOn);
+            }
+            
+        } else
+        {
+            isOn = false;
+            for (int i = 0; i < arrayOfPartyLights.Length; i++)
+            {
+                arrayOfPartyLights[i].SetActive(isOn);
+            }
+        }
+    }
+
+    
+}
